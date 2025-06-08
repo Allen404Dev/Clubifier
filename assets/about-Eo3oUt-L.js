84e37b8cf180f71c8b1f865c2f32fb64769ef94f
codingschool_aabdijanovic <Allen.Abdijanovic@edu.fh-kaernten.ac.at>
2025-06-08 22:43:30 +0200

    Deploy

---
+++ assets/about-Eo3oUt-L.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CGwYWsz8.js";const n=function(){return o.jsx("div",{className:"p-2 bg-black text-blue-500",children:"Hello from About!"})};export{n as component};
--- assets/index-WRQuoDYj.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C__aKR3a.js";const t=function(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{className:"text-blue",children:"Welcome Home!"})})};export{t as component};
